792b6613c92a79b2c5f12eb8b788b45f2ee364ad
Eric Hodel <drbrain@segment7.net>
2017-11-09 11:02:19 -0800

    Implement part 1

--- 1.rb
+++ 1.rb
@@ -1,5 +1,33 @@
 require_relative "aoc"
 
-input 1 do |input|
-  puts input
+directions = %i[north east south west]
+x, y = 0, 0
+direction = 0
+
+input 1, split: "," do |input|
+  /(?<turn>.)(?<blocks>.+)/ =~ input
+
+  blocks = Integer blocks
+
+  direction +=
+    if turn == "L" then
+      -1
+    else
+      1
+    end
+
+  direction %= 4
+
+  case direction
+  when 0 then
+    x += blocks
+  when 1 then
+    y += blocks
+  when 2 then
+    x -= blocks
+  else
+    y -= blocks
+  end
 end
+
+puts x.abs + y.abs
