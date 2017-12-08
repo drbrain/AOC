d0b4caadd98f0907fddd14ca85b1166f86bcc5e9
Eric Hodel <drbrain@segment7.net>
2017-12-07 21:14:38 -0800

    Day 8

---
+++ 2017/8b.rb
@@ -0,0 +1,34 @@
+require_relative "../aoc"
+
+test <<INSTRUCTIONS, 10
+b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10
+INSTRUCTIONS
+
+input 2017, 8 do |input|
+  max = 0
+  registers = Hash.new 0
+
+  input.lines.each do |line|
+    /(\S+) (inc|dec) (\S+) if (\S+) (\S+) (\S+)/ =~ line
+
+    register, operation, amount, condition_register, condition, value =
+      $1, $2, Integer($3), $4, $5, Integer($6)
+
+    operation =
+      if "inc" == operation then
+        :+
+      else
+        :-
+      end
+
+    result = registers[condition_register].send condition, value
+
+    registers[register] = registers[register].send operation, amount if result
+    max = [max, registers.values.max].compact.max
+  end
+
+  p max
+end
