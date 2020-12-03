b213608514156695db36013efcc27bf863769749
Eric Hodel <drbrain@segment7.net>
2020-12-02 21:28:00 -0800

    Day 3 2020 part B

--- 2020/src/bin/day_3.rs
+++ 2020/src/bin/day_3.rs
@@ -9,13 +9,28 @@ fn main() -> Result<()> {
 
     println!("part A: {}", day_3_a(&input, 3, 1)?);
 
+    println!("part B: {}", day_3_b(&input)?);
+
     Ok(())
 }
 
-fn day_3_a(input: &String, run: usize, drop: usize) -> Result<u32> {
+fn day_3_a(input: &String, run: usize, drop: usize) -> Result<u64> {
     let slope = Slope::from(input);
 
     Ok(slope.toboggan(run, drop)?)
+}
+
+fn day_3_b(input: &String) -> Result<u64> {
+    let slope = Slope::from(input);
+
+    let slopes = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+
+    let answer = slopes
+        .into_iter()
+        .map(|(run, drop)| slope.toboggan(run, drop).unwrap())
+        .fold(1, |a, b| a * b);
+
+    Ok(answer)
 }
 
 #[derive(Debug)]
@@ -24,7 +39,7 @@ struct Slope {
 }
 
 impl Slope {
-    fn toboggan(&self, run: usize, drop: usize) -> Result<u32> {
+    fn toboggan(&self, run: usize, drop: usize) -> Result<u64> {
         let mut row = drop;
         let mut col = run;
         let width = self.rows[0].len();
@@ -76,6 +91,25 @@ mod test {
 .#..#...#.#",
         );
 
-        assert_eq!(7, day_3_a(input, 3, 1).unwrap());
+        assert_eq!(7, day_3_a(&input, 3, 1).unwrap());
+    }
+
+    #[test]
+    fn test_day_3_b() {
+        let input = String::from(
+            "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#",
+        );
+
+        assert_eq!(336, day_3_b(&input).unwrap());
     }
 }
